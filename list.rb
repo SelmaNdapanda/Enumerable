e1152f329bfe3ba9ab6f771c965a8b86877c41fa
KareemWilson <kareemmagdywilson@gmail.com>
2022-12-22 16:39:14 +0200

    Fix Linters Errors

--- list.rb
+++ list.rb
@@ -1,22 +1,21 @@
-require './enumerable.rb'
+require './enumerable'
 
 class MyList
-    def initialize(list)
-        @list = list
-    end
+  def initialize(list)
+    @list = list
+  end
 
-    include MyEnumerable
+  include MyEnumerable
 
-    def each(&block)
-        @list.each(&block)
-    end
+  def each(&block)
+    @list.each(&block)
+  end
 end
-
 
 list = MyList.new([1, 2, 3, 4])
 
-puts list.all? {|e| e < 5}
-puts list.all? {|e| e > 5}
-puts list.any? {|e| e == 2}
-puts list.any? {|e| e == 5}
-puts list.filter {|e| e.even?}
+p(list.all? { |e| e > 5 })
+p(list.all? { |e| e > 5 })
+p(list.any? { |e| e == 2 })
+p(list.any? { |e| e == 5 })
+p(list.filter(&:even?))
